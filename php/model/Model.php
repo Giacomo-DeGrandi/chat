df366d37ad054233d7054239b620ed146e992cff
giacomo-degrandi <carlo.de-grandi-giacomo@laplateforme.io>
2022-05-20 07:55:39 +0200

    plesk test now

--- php/model/Model.php
+++ php/model/Model.php
@@ -8,18 +8,19 @@ abstract class Model {
     function connect(){
 
 
+        /*
         $server="localhost";
         $username="root";
         $password="";
         $database="new_discord";
+                    */
 
 
-        /*
         $server="localhost:3306";
         $username="digitree";
         $password="@LaPlateforme92.@";
         $database="carlo-de-grandi-giacomo_little_discord";
-            */
+
 
         $dsn = "mysql:host=$server;dbname=$database;charset=UTF8";
         $this->conn = new PDO($dsn, $username, $password);
